8cac520577eebe9fca2aabe78ba75378e2e2de6d
Mike-415 <mgonz160@mail.ccsf.edu>
2023-02-08 13:11:45 -0800

    Added banner image

--- app/src/main/java/com/example/androidcomposeassignment1/MainActivity.kt
+++ app/src/main/java/com/example/androidcomposeassignment1/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.androidcomposeassignment1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androidcomposeassignment1.ui.theme.AndroidComposeAssignment1Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +30,35 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Box() {
+                        BannerImage()
+                        Title()
+                    }
+
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Title(title: String){
+    Text(text = title,
+        fontSize = 24.sp,
+        modifier = Modifier.padding(16.dp))
+}
+@Composable
+fun BannerImage(){
+    Image(
+        painter = painterResource(id = R.drawable.img),
+        contentDescription = null,
+        alignment = Alignment.TopStart
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     AndroidComposeAssignment1Theme {
-        Greeting("Android")
+        BannerImage()
     }
 }
